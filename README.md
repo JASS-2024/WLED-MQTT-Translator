98344188a2fc43e41e630f2f93b26c731859099a
Hamish Starling <hamishstarling@hotmail.co.uk>
2024-04-04 19:39:25 +0300

    Implement MQTT Translator

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # WLED-MQTT-Translator
-Transmits the live LED status (WLED WS data) over MQTT to allow more than 1 user to have it at once. 
+Retransmits the live LED status (WLED WS data) over MQTT to allow more than 1 user to have it at once. 
---
+++ translator.py
@@ -0,0 +1,33 @@
+import paho.mqtt.client as mqtt
+import websocket
+import threading
+
+from datetime import datetime
+
+BROKER_ADDRESS = "192.168.3.85"
+BROKER_PORT = 1883
+# WLED_WS_ADDRESS = "ws://192.168.0.48/ws"
+WLED_WS_ADDRESS = "ws://192.168.0.33/ws"
+TOPIC = "WLEDTranslator/48"
+
+
+mqtt_client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+seen_message = False
+
+def on_open(ws):
+    mqtt_client.connect(BROKER_ADDRESS, BROKER_PORT, 60)
+    ws.send("{'lv':true}")
+
+def on_close(ws):
+    mqtt_client.disconnect()
+    print("WebSocket connection closed")
+
+def forward_message(ws, message):
+    global seen_message
+    print(f"Received Message {datetime.utcnow().microsecond}")
+    last_message = True
+    mqtt_client.publish(TOPIC, message)
+
+ws = websocket.WebSocketApp(WLED_WS_ADDRESS, on_message=forward_message, on_open=on_open, on_close=on_close)
+while True:
+    ws.run_forever()
